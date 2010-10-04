7bd1d67ac3c43626a1a93dfd0681961b576ee729
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2010-10-04 16:07:50 +0200

    Refactored test. Removed redundant test scenario.

--- appseed.metrix/src/test/groovy/org/appseed/metrix/AValueDomain.groovy
+++ appseed.metrix/src/test/groovy/org/appseed/metrix/AValueDomain.groovy
@@ -4,33 +4,26 @@ import spock.lang.Specification;
 
 class AValueDomain extends Specification {
 	
-	def 'is defined over a class and has a name'(){
-		when: 'a domain "distance" is created over the class Number and enabled'
-			def distance=new ValueDomain(name:'distance', over:Number) 
-			distance.enabled=true
-		then: 'a proper domain object is built and class Number has a read only property called "distance"'
-			distance.name == 'distance'
-			distance.over == Number
-			1.distance
-		
-		when: '"distance" domain is disabled'
-			distance.enabled=false
-			1.distance
-		then: 'class Number has not "distance" property any more'
-			thrown(MissingPropertyException)
-	}
-	
-	def 'can be defined over any class and each class instance has a read only property returning the domain'(){
+	def 'defined over a class, adds to that class\' instances a read only property returning the domain, only when enabled'(){
 		given: 'a domain "distance" created over a class and enabled'
 			def distance=new ValueDomain('distance', aClass, true)
 		expect: 'every instance of that class has a distance property returning the "distance" domain'
 			aValue.distance == distance
 		
-		cleanup:
+		when: 'the domain is disabled and the read only property is accessed'
 			distance.enabled=false
-		
+			aValue.distance
+		then: 'a missing property exception is thrown'
+			thrown(MissingPropertyException)
+			
 		where:
-			aValue << [-1.7, 1.7, "a string", true, false, 467F, 12]
-			aClass << [Number, Number, String, Boolean, Boolean, Float, Integer]
+			aValue     | aClass
+			-1.7       | Number
+			 1.7       | Number
+			'a string' | String
+			 true      | Boolean
+			 false     | Boolean
+			 467F      | Float
+			 12        | Integer
 	}
 }
