d767dc138e9537f8808a02aca0a082b9652cc349
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2010-10-04 17:08:10 +0200

    We can define two different ValueDomain over same class without interference, each one will have its own property

--- appseed.metrix/src/main/groovy/org/appseed/metrix/ValueDomain.groovy
+++ appseed.metrix/src/main/groovy/org/appseed/metrix/ValueDomain.groovy
@@ -1,30 +1,26 @@
 package org.appseed.metrix
 
-import groovy.lang.ExpandoMetaClass;
+import groovy.lang.Delegate;
 
+
+/* IMPLEMENTATION NOTE:
+ * Use on the fly properties instead of ExpandoMetaClass for the following reasons:
+ * - Seems there are no way to remove a property/getter from an ExpandoMetaClass
+ * - Worried about the ThreadLocal issue
+ * - If you enable ExpandoMetaClass.enableGlobally() it won't work !
+ */
 class ValueDomain {
+	@Lazy(soft=false) private static final DOMAINS=[:];
+	
 	static {
 		ExpandoMetaClass.disableGlobally()
 	}
-	boolean enabled;
-	private String name;
-	private Class over;
 	
-	/* IMPLEMENTATION NOTE:
-	 * Use on the fly properties instead of ExpandoMetaClass for the following reasons:
-	 * - Seems there are no way to remove a property/getter from an ExpandoMetaClass
-	 * - Worried about the ThreadLocal issue
-	 * - If you enable ExpandoMetaClass.enableGlobally() it won't work !
-	 */
-	def ValueDomain(String name, Class over, enabled=false) {
-		super()
-		this.name=name;
-		this.over=over;
-		this.enabled=enabled;
-		def thisDomain=this;
-		this.over.metaClass.getProperty << { String propName ->
-			if(this.enabled && this.name == propName)
-				return this
+	private static addDomainPropertyToClass(Class over) {
+		over.metaClass.getProperty << { String propName ->
+			def domain=DOMAINS[propName]
+			if(domain&&domain.over.isAssignableFrom(delegate.class))
+				return domain
 			def metaProp = delegate.metaClass.getMetaProperty(propName)
 			if (metaProp) {
 				return metaProp.getValue(delegate)
@@ -34,17 +30,42 @@ class ValueDomain {
 		}
 	}
 	
-	def ValueDomain(definition) {
-		this(definition.name, definition.over)
-		if(definition.enabled)
-			enabled=definition.enabled
+	private boolean enabled;
+	private String name;
+	private Class over;
+	
+	def ValueDomain(String name, Class over, enabled=false) {
+		super()
+		this.name=name;
+		this.over=over;
+		setEnabled(enabled);
+		addDomainPropertyToClass over
 	}
 	
+	def ValueDomain(definition) {
+		this(definition.name, definition.over, definition.enabled?:false)
+	}
+		
 	def getName() {
-		name;
+		name
 	}
 	
 	def getOver() {
-		over;
+		over
+	}
+	
+	def getEnabled() {
+		enabled
+	}
+	
+	def setEnabled(isEnabled) {
+		def newEnabled=isEnabled as Boolean
+		if(enabled!=newEnabled) {
+			enabled=newEnabled
+			if(enabled)
+				DOMAINS[name]=this
+			else
+				DOMAINS.remove(name)
+		}
 	}
 }
--- appseed.metrix/src/test/groovy/org/appseed/tests/metrix/AValueDomain.groovy
+++ appseed.metrix/src/test/groovy/org/appseed/tests/metrix/AValueDomain.groovy
@@ -35,20 +35,44 @@ class AValueDomain extends Specification {
 		when: 'the read only property is accessed in a non realated class instance'
 			aValue.distance
 		then: 'a missing property exception is thrown'
+			aClassInstance.distance == distance
 			thrown(MissingPropertyException)
 			
 		cleanup:
-			distance.enabled=false;
+			distance.enabled=false
 		
 		where:
-			aValue     | aClass
-			-1.7       | Integer
-			 1.7       | String
-			'a string' | Boolean
-			 true      | String
-			 false     | Float
-			 467F      | Integer
-			 12        | Double
+			aValue     | aClass  | aClassInstance
+			-1.7       | Integer | 1
+			 1.7       | String  | 'pepe'
+			'a string' | Boolean | true
+			 true      | String  | 'juan'
+			 false     | Float   | 45F
+			 467F      | Integer | 33
+			 12        | Double	 | 34D
+	}
+	
+	def 'defined over a class and other value domain over the same class will not interfere each other'(){
+		given: 'a domain "distance" and other "weight", defined over the same class and both enabled'
+			def distance=new ValueDomain('distance', aClass, true)
+			def weight=new ValueDomain('weight', aClass, true)
+		expect: 'every instance of that class has a distance property returning the "distance" domain and a weight property returning the "weight" domain'
+			weight != distance
+			aValue.distance == distance
+			aValue.weight == weight
 		
+		cleanup:
+			distance.enabled=false
+			weight.enabled=false
+			
+		where:
+			aValue     | aClass
+			-1.7       | Number
+			 1.7       | Number
+			'a string' | String
+			 true      | Boolean
+			 false     | Boolean
+			 467F      | Float
+			 12        | Integer
 	}
 }
